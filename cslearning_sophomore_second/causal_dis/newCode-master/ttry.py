f04b5326b0da307b4635421b6b24b18f96e08481
gnahz <161614928+SunkInDream@users.noreply.github.com>
2025-07-05 23:30:38 +0800

    Delete cslearning_sophomore_second/causal_dis/newCode-master directory

--- cslearning_sophomore_second/causal_dis/newCode-master/ttry.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from models_impute import impute
-from baseline import *
-data = pd.read_csv('ICU_Charts/200003.csv').values
-print(data)
-data_imputed = tefn_impu(data)
-print(data_imputed)
-pd.DataFrame(data_imputed).to_csv('test.csv')
